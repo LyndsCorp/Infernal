047c43888d83d196ceb454b6af755a4f2fbfd9c6
David Baña Szymaniak <monojo.lynds@gmail.com>
2026-07-31 16:36:30 +0200

    Nuevos alias para crear funciones y comentarios de qué hace cada token

--- src/lexer/keywords.c
+++ src/lexer/keywords.c
@@ -7,6 +7,7 @@
 #include "keywords.h"
 
 TokenType lookup_keyword(const char *lexeme) {
+    // Control de flujo
     if (strcmp(lexeme, "if") == 0) return TOK_IF;
     if (strcmp(lexeme, "then") == 0) return TOK_THEN;
     if (strcmp(lexeme, "fi") == 0) return TOK_FI;
@@ -14,27 +15,48 @@ TokenType lookup_keyword(const char *lexeme) {
     if (strcmp(lexeme, "elseif") == 0) return TOK_ELSEIF;
     if (strcmp(lexeme, "while") == 0) return TOK_WHILE;
     if (strcmp(lexeme, "for") == 0) return TOK_FOR;
+
+    // Funciones
     if (strcmp(lexeme, "function") == 0) return TOK_FUNCTION;
+    if (strcmp(lexeme, "func") == 0) return TOK_FUNCTION;
+    if (strcmp(lexeme, "fn") == 0) return TOK_FUNCTION;
+    if (strcmp(lexeme, "def") == 0) return TOK_FUNCTION;
+
     if (strcmp(lexeme, "return") == 0) return TOK_RETURN;
+
+    // Bucles
     if (strcmp(lexeme, "break") == 0) return TOK_BREAK;
     if (strcmp(lexeme, "repeat") == 0) return TOK_REPEAT;
     if (strcmp(lexeme, "continue") == 0) return TOK_CONTINUE;
+
+    // Módulos Fire
     if (strcmp(lexeme, "import") == 0) return TOK_IMPORT;
+
+    // Robustez
     if (strcmp(lexeme, "try") == 0) return TOK_TRY;
     if (strcmp(lexeme, "catch") == 0) return TOK_CATCH;
+
+    // Tipos de variables
     if (strcmp(lexeme, "int") == 0) return TOK_INT;
     if (strcmp(lexeme, "float") == 0) return TOK_FLOAT;
     if (strcmp(lexeme, "bool") == 0) return TOK_BOOL;
     if (strcmp(lexeme, "string") == 0) return TOK_STRING;
     if (strcmp(lexeme, "list") == 0) return TOK_LIST;
+
+    // Booleanos
     if (strcmp(lexeme, "true") == 0) return TOK_TRUE;
     if (strcmp(lexeme, "false") == 0) return TOK_FALSE;
+
+    // Scopes para variables (si está vacío, es una variable normal)
     if (strcmp(lexeme, "local") == 0) return TOK_LOCAL;
     if (strcmp(lexeme, "global") == 0) return TOK_GLOBAL;
+
+    // Condiciones
     if (strcmp(lexeme, "and") == 0) return TOK_AND;
     if (strcmp(lexeme, "or") == 0) return TOK_OR;
     if (strcmp(lexeme, "in") == 0) return TOK_IN;
-    if (strcmp(lexeme, "flag") == 0) return TOK_FLAG;
+
+    // Utilidades
     if (strcmp(lexeme, "flags") == 0) return TOK_FLAG;
     if (strcmp(lexeme, "line") == 0) return TOK_LINE;
     return TOK_IDENT;
