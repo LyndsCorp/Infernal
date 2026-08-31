ddeb49f5b810d36cd66f089549029f8372ecfe4b
David Baña Szymaniak <monojo.lynds@gmail.com>
2026-08-31 23:34:58 +0200

    Actualizo el TODO y corrijo la cabecera de main.c, solo comentarios

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
  * Infernal: el intérprete de Aro Infernal.
  * Copyright (C) 2026, David Baña Szymaniak
  * Este software se distribuye bajo la licencia Apache 2.0
- * Código fuente de Infernal: vm/main.c
+ * Código fuente de Infernal: main.c
 */
 
 #include <stdio.h>
